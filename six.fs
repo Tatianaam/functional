0059879f0f9342b683ef38b9d4577f702bf7c01d
Tatianaam <tatiana28am@gmail.com>
2022-10-05 17:28:21 -0400

    fix

--- six.fs
+++ six.fs
@@ -5,10 +5,12 @@ let rec pow = function
 | (s, n) -> s + pow(s, n-1)
 
 //17.2
-let rec isIthChar (s, n, c) = (string s).[n] =  c
+let rec isIthChar = function 
+| (s, n, c) when n < 0 || n >= String.length(s) || s.[n] <> c -> false
+| (s, n, c) -> true
 
 //17.3
 let rec occFromIth = function
-| (s, n, c)  when n >= String.length(s) -> 0
+| (s, n, c)  when n < 0 || n >= String.length(s) -> 0
 | (s, n, c) when s.[n] <> c -> 0 + occFromIth(s, n + 1, c)
 | (s, n, c) -> 1 + occFromIth(s, n + 1, c)
