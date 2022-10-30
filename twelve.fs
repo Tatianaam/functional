18aa9e4eed06ac5a804f7ada09722dd835df4394
tatianaam <tatiana28am@gmail.com>
2022-10-30 15:09:00 -0400

    twelve

---
+++ twelve.fs
@@ -0,0 +1,24 @@
+// 34.1
+let rec helper = function
+| (c, n, l) when c = n -> helper(c-1, n , [n]) 
+| (c, n, l) when c = 0 -> l
+| (c, n, l) -> helper(c-1, n, c :: l) 
+
+let rec upto n = if n <= 0 then [] else helper(n, n, [])
+
+//34.2
+let rec helperD = function
+| (c, n, l) when c = n -> c :: l
+| (c, n, l) when c = 1 -> helperD(c+1, n , [1]) 
+| (c, n, l) -> helperD(c+1, n, c :: l) 
+
+
+let rec dnto n = if n <= 0 then [] else helperD(1, n, [])
+
+// 34.3
+let rec helperE = function
+| (c, n, l) when n = 2 -> [2]
+| (c, n, l) when c = 2 -> c :: l
+| (c, n, l) -> helperE(c-2, n, c :: l)
+
+let rec evenn n = if n <= 0 then [] else helperE (2*n, 2*n, [])
