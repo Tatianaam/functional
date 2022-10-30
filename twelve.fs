cda610c9d5e75198338c62d80c23afbc054ed9ac
tatianaam <tatiana28am@gmail.com>
2022-10-30 19:17:33 -0400

    update

--- twelve.fs
+++ twelve.fs
@@ -17,8 +17,8 @@ let rec dnto n = if n <= 0 then [] else helperD(1, n, [])
 
 // 34.3
 let rec helperE = function
-| (c, n, l) when n = 2 -> [2]
-| (c, n, l) when c = 2 -> c :: l
+| (c, n, l) when c = 0 -> 0 :: l
+| (c, n, l) when c = n -> helperE(c-2, n, c :: l)
 | (c, n, l) -> helperE(c-2, n, c :: l)
 
-let rec evenn n = if n <= 0 then [] else helperE (2*n, 2*n, [])
+let rec evenn n = if n <= 0 then [] else helperE (n*2-2, n*2-2, [])
