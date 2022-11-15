5eb2eae5ed35850ef6fd4845c2fa439021cdd03d
tatianaam <tatiana28am@gmail.com>
2022-11-14 21:42:39 -0500

    folds

---
+++ fifteen.fs
@@ -0,0 +1,15 @@
+// 41.4.1.
+let list_filter f xs = List.foldBack (fun cur prev -> if f cur then cur :: prev else prev) xs []
+
+// 41.4.2
+let sum (p, xs) = List.foldBack (fun cur prev -> if p cur then cur+prev else prev) xs 0
+
+// 41.4.3
+let rev lst  = List.fold(fun head tail -> tail :: head) [] lst
+
+let revrev = fun x -> List.fold(fun head tail -> rev tail :: head) [] x
+
+
+-----------------------------------------
+//let testRev lst = List.foldBack(fun head tail -> tail @ [head]) lst []
+//let testRevRev lst = List.foldBack(fun head tail -> tail @ [(List.fold(fun head tail -> tail :: head) [] head)] ) lst []
